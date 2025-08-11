06ed872c5f3dd23f48e3151b74be4600b636b368
Nicolas Metais <nico.metais@hotmail.fr>
2025-08-11 21:58:05 +0200

    yes

--- apps/api/dist/routes/default.js
+++
@@ -1,7 +0,0 @@
-export default async function (fastify, opts) {
-    const db = opts.db;
-    fastify.get('/test', async () => {
-        const users = await db.all('SELECT * FROM users');
-        return users;
-    });
-}
